0622b7b2f14cadf95bc465a17e53dca25d51885b
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2026-05-23 19:52:36 +0530

    update files

---
+++ search_resulr.php
@@ -0,0 +1,9 @@
+<?php
+ include ('config.php');
+
+ if(isset($_POST['submit'])){
+    $fullName = $_POST['Ful_name'];
+    $nic = $_POST['Nic'];
+    $address = $_POST['Address'];
+
+    $result = mysqli_query($conn, "SELECT * FROM resident WHERE fullName LIKE '%$fullName%' AND nic='$nic' AND address LIKE '%$address%'");
