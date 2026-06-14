c23f41e87a5b2dfe945e8e2f2823a15ba0938256
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2026-06-14 19:28:30 +0530

    update modify file

---
+++ modify.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modify</title>
+</head>
+<body>
+    <?php
+          session_start();
+          if(isset($_session['row_date'])){
+            $row=$_session['row_date'];
+          }
+          ?>
+</body>
+</html>
