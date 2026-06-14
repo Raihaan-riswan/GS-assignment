bb33ae38b501812e7db9d1b228f6dfbb5dea27db
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2026-06-14 20:49:51 +0530

    update modify file

--- modify.php
+++ modify.php
@@ -17,7 +17,7 @@
               <h3>Modify Details</h3>
 
 
-              <label>Full Name: <input type="text" id="fullname" name="fullname" value="<?php echo $row['fullname']; ?>"></label>
+              <label>Full Name: <input type="text" id="full_name" name="full_name" value="<?php echo $row['full_name']; ?>"></label>
 
               <label>Date of Birth: <input type="text" id="dob" name="dob" value="<?php echo $row['dob']; ?>"></label>
 
